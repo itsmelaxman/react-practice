1e0fe0f831dab050dcfb57cd9b6b129fc4af51d6
Laxman Magarati <Laxmanmagrati@gmail.com>
2023-07-16 13:17:16 +0545

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+
+import Button from './Button';
+
+const App=()=> {
+    return (
+          <div>
+             <h1>Useful Website</h1>
+             <h3> Which website do you want to open</h3>
+          <Button name="Google" link="https://google.com"/>
+          <Button name="DN Website" link="https://edigitalnepal.com"/>
+          <Button name="Github" link="https://github.com"/>
+          </div>
+    )
+ }
+ 
+ export default App;
